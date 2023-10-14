995709bb6eeabfcd3ce44e328f97fbb10a2bda67
aunurrofiqFaried <aunurrofiqWajdi@gmail.com>
2023-10-14 20:54:34 +0700

    step 15 Menambahkan NewWordActivity

--- app/src/main/java/com/example/roomcorotin/WordRoomDatabase.kt
+++ app/src/main/java/com/example/roomcorotin/WordRoomDatabase.kt
@@ -58,11 +58,12 @@ public abstract class WordRoomDatabase : RoomDatabase() {
                     context.applicationContext,
                     WordRoomDatabase::class.java,
                     "word_database"
-                ).build()
+                )
+                    .addCallback(WordDatabaseCallback(scope))
+                    .build()
                 INSTANCE = instance
                 // return instance
-                instance
-            }
+                instance            }
         }
     }
 }
